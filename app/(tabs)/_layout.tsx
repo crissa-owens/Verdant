dbbdfe4e064f0e5b31a87532768ad89918a12e9a
Kevin Pinkett <kpinkett@udel.edu>
2026-02-28 21:25:33 -0500

    page routing fixed

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,19 +1,28 @@
-import { Tabs } from 'expo-router';
+import { router, Tabs } from 'expo-router';
 import React from 'react';
 import { IconSymbol } from '../components/ui/icon-symbol';
 import { HapticTab } from '../components/haptic-tab';
+import { TouchableOpacity } from 'react-native';
 
 export default function TabLayout() {
 
   return (
     <Tabs
       screenOptions={{
+              headerTitle: "Verdant",
+              headerTitleAlign: "center",
+              headerRight: () => 
+              <TouchableOpacity
+                onPress={() => router.push("/profile")}
+                style={{ marginRight: 15 }}>
+                <IconSymbol name="person.circle" size={28} color="black" />
+              </TouchableOpacity>,
               tabBarActiveTintColor: "green",
               headerShown: true,
               tabBarButton: HapticTab,
             }}>
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
@@ -26,6 +35,16 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="list.bullet" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="shop"
+        options={{
+          title: 'Shop',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="cart" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{ href: null}}/>
     </Tabs>
   );
 }
