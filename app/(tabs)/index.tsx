dbe87bc7b68053cc1c9130fb6aa0642571608d89
Kevin Pinkett <kpinkett@udel.edu>
2026-03-01 03:13:06 -0500

    unfavorite tasks

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,12 @@
 import { useContext } from "react";
 import { Text, View } from "react-native";
-import ProgressBar from "../components/progress-bar";
 import TaskWidget from "../components/task-widget";
 import { NameContext } from "../context/name";
-import { TaskContext } from "../context/task";
 import { styles } from "../styles";
 
 export default function HomePage() {
   const nameCtxt = useContext(NameContext);
-  const taskCtxt = useContext(TaskContext);
   if (!nameCtxt) throw new Error("NameContext missing");
-  if (!taskCtxt) throw new Error("TaskContext missing");
 
   return (
     <View style={{ flex: 1 }}>
@@ -20,7 +16,7 @@ export default function HomePage() {
       </View>
 
       <View style={{ alignSelf: "center", alignItems: "center", width: "70%" }}>
-        <TaskWidget task={taskCtxt.task} />
+        <TaskWidget/>
       </View>
     </View>
   );
--- app/components/task-widget.tsx
+++ app/components/task-widget.tsx
@@ -1,15 +1,22 @@
-import { View, Text, StyleSheet } from "react-native";
-import { Task } from "../interfaces/task";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { IconSymbol } from "./ui/icon-symbol";
+import { useContext } from "react";
+import { TaskContext } from "../context/task";
 
-interface TaskWidgetProps {
-    task: Task | null;
-}
+export default function TaskWidget() {
+    const taskCtxt = useContext(TaskContext);
+    if (!taskCtxt) throw new Error("TaskContext missing");
+    
+    const { task } = taskCtxt;
 
-export default function TaskWidget({ task }: TaskWidgetProps) {
     if (task) {    
     return (
             <View style={styles.favorite_task}>
                 <Text style={{ fontWeight: "bold", marginBottom: 10 }}>{task.Title}</Text>
+                <TouchableOpacity style={{ position: "absolute", top: 5, right: 10 }}
+                onPress={() => {taskCtxt.setTask(null)}}>
+                    <IconSymbol name="star" size={36} color="#f2c910" />
+                </TouchableOpacity>
                 <Text style={{ marginBottom: 10 }}>{task.Description}</Text>
                 <Text style={{ fontStyle: "italic" }}>
                     {task.SproutValue} Sprouts per {task.Threshold} {task.Unit}
@@ -27,6 +34,7 @@ export default function TaskWidget({ task }: TaskWidgetProps) {
 
 const styles = StyleSheet.create({
     favorite_task: {
+        position: "relative",
         alignItems: "center",
         justifyContent: "center",
         backgroundColor: "#e6e6e6",
