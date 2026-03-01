070c59e0e2ecc9d4ffaa925e2e810bbc587bac0d
sschiff@udel.edu <sschiff@udel.edu>
2026-03-01 07:06:04 -0500

    added detail to events-tasks

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,6 @@
 import { router, Tabs } from "expo-router";
 import React from "react";
 import { TouchableOpacity } from "react-native";
-<<<<<<< HEAD
-import { HapticTab } from "../components/haptic-tab";
-import { SproutCounter } from "../components/counter";
-import { IconSymbol } from "../components/ui/icon-symbol";
-import { NameContext } from "../context/name";
-import { SproutContext } from "../context/sprout";
-=======
 import { SproutCounter } from "../components/counter";
 import { HapticTab } from "../components/haptic-tab";
 import { IconSymbol } from "../components/ui/icon-symbol";
@@ -17,7 +10,6 @@ import { SproutContext } from "../context/sprout";
 import { TaskContext } from "../context/task";
 import { Reward } from "../interfaces/reward";
 import { Task } from "../interfaces/task";
->>>>>>> origin/main
 
 export default function TabLayout() {
   const [sprouts, setSprouts] = React.useState(0);
@@ -28,68 +20,6 @@ export default function TabLayout() {
   return (
     <SproutContext.Provider value={{ sprouts, setSprouts }}>
       <NameContext.Provider value={{ name, setName }}>
-<<<<<<< HEAD
-        <Tabs
-          screenOptions={{
-            headerTitle: "Verdant",
-            headerTitleAlign: "center",
-            headerLeft: () => <SproutCounter count={sprouts} />,
-            headerRight: () => (
-              <TouchableOpacity
-                onPress={() => router.push("/profile")}
-                style={{ marginRight: 15 }}
-              >
-                <IconSymbol name="person.circle" size={28} color="black" />
-              </TouchableOpacity>
-            ),
-            tabBarActiveTintColor: "green",
-            headerShown: true,
-            tabBarButton: HapticTab,
-          }}
-        >
-          <Tabs.Screen
-            name="index"
-            options={{
-              title: "Home",
-              tabBarIcon: ({ color }) => (
-                <IconSymbol size={28} name="house" color={color} />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="tasks"
-            options={{
-              title: "Tasks",
-              tabBarIcon: ({ color }) => (
-                <IconSymbol size={28} name="list.bullet" color={color} />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="shop"
-            options={{
-              title: "Shop",
-              tabBarIcon: ({ color }) => (
-                <IconSymbol size={28} name="cart" color={color} />
-              ),
-            }}
-          />
-          <Tabs.Screen
-            name="community"
-            options={{
-              title: "Community",
-              tabBarIcon: ({ color }) => (
-                <IconSymbol
-                  size={28}
-                  name="bubble.left.and.text.bubble.right"
-                  color={color}
-                />
-              ),
-            }}
-          />
-          <Tabs.Screen name="profile" options={{ href: null }} />
-        </Tabs>
-=======
         <TaskContext.Provider value={{ task, setTask }}>
           <RewardContext.Provider value={{ reward, setReward }}>
             <Tabs
@@ -115,7 +45,7 @@ export default function TabLayout() {
                 options={{
                   title: "Home",
                   tabBarIcon: ({ color }) => (
-                    <IconSymbol size={28} name="house.fill" color={color} />
+                    <IconSymbol size={28} name="house" color={color} />
                   ),
                 }}
               />
@@ -141,7 +71,6 @@ export default function TabLayout() {
             </Tabs>
           </RewardContext.Provider>
         </TaskContext.Provider>
->>>>>>> origin/main
       </NameContext.Provider>
     </SproutContext.Provider>
   );
