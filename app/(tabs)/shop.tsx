b8e40b551c9a699da95af5370ed4dff5b1acab2a
Kevin Pinkett <kpinkett@udel.edu>
2026-02-28 20:38:31 -0500

    error caught

--- app/(tabs)/shop.tsx
+++ app/(tabs)/shop.tsx
@@ -1,28 +1,32 @@
+import React from "react";
 import { ScrollView, Text, View } from "react-native";
 import Divider from "../components/divider";
 import ShopCard from "../components/shop-card";
 import { Reward } from "../interfaces/reward";
 import { styles } from "../styles";
-import React from "react";
-
-
-import rewardData from "../data/rewards.json";
 import ExpandedReward from "../components/expanded-reward";
+import rewardData from "../data/rewards.json";
+
 const REWARDS: Reward[] = rewardData.rewards;
 
 export default function ShopPage() {
-    const [selectedReward, setSelectedReward] = React.useState<Reward | null>(null);
-    function handleRewardPress(reward: Reward) {
-        setSelectedReward(reward);
-    }
+  const [selectedReward, setSelectedReward] = React.useState<Reward | null>(
+    null,
+  );
+  function handleRewardPress(reward: Reward) {
+    setSelectedReward(reward);
+  }
 
-    if (selectedReward) {
-        return (
-            <ExpandedReward reward={selectedReward} onBack={() => setSelectedReward(null)}/>
-        )
-    }
-
+  if (selectedReward) {
     return (
+      <ExpandedReward
+        reward={selectedReward}
+        onBack={() => setSelectedReward(null)}
+      />
+    );
+  }
+
+  return (
     <View style={{ flex: 1 }}>
       <View style={styles.shop_styles}>
         <Text style={styles.title}>Shop</Text>
@@ -31,11 +35,6 @@ export default function ShopPage() {
           {REWARDS.map((reward, index) => (
             <ShopCard key={index} reward={reward} onPress={handleRewardPress} />
           ))}
-          {selectedReward && (
-            <View style={{ padding: 20 }}>
-              <Text style={{ fontSize: 24, fontWeight: "bold" }}>{selectedReward.title}</Text>
-            </View>
-          )}
         </ScrollView>
       </View>
     </View>
