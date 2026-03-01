b5882c1e6237d2295edf21898f1c13e1853a6f8c
sschiff@udel.edu <sschiff@udel.edu>
2026-03-01 03:41:43 -0500

    Loading page done actually this time

--- app/components/loading.tsx
+++ app/components/loading.tsx
@@ -55,13 +55,3 @@ const styles = StyleSheet.create({
 });
 
 export default LoadingDots;
-//export default function App() {
-//    return (
-//        <View style ={{flex: 1, justifyContent:"center", alignItems:"center", backgroundColor: "lightslategrey"}}>
-//            <Image source={{uri: 'https://i.imgur.com/d11lUJY.png'}}
-//            style={{ width: 200, height: 200, }}/>
-//            <Text style = {{fontSize: 50}}>Loading...</Text>
-//            <Text style = {{fontSize: 20}}>Thank you for using Verdant!</Text>
-//        </View>
-//    );
-//}
