7b3446e98a9d3b82d6fbe13289692a4453082cb9
Kevin Pinkett <kpinkett@udel.edu>
2026-02-28 20:45:15 -0500

    profile created, index renamed to home

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,10 @@
+import { ScrollView, Text, View } from "react-native";
+import { styles } from "../styles";
+
+export default function Profile() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Profile</Text>
+        </View>
+    )
+}
